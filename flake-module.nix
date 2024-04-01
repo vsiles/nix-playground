e301103f338aefc3426e7a7d3522d54aaf510f38
Vincent Siles <vincent.siles@gmail.com>
2024-04-01 10:15:46 +0200

    commiting stuff

--- flake-module.nix
+++ flake-module.nix
@@ -1,9 +1,9 @@
 { ... }: {
   imports = [
-    ./14-rust-docker-build-publish/manifest.nix
-    ./14-rust-docker-build-publish/rust.nix
-    ./14-rust-docker-build-publish/docker.nix
-    ./14-rust-docker-build-publish/all.nix
-    ./14-rust-docker-build-publish/publish.nix
+    ./14-rust-docker-build-all-publish/manifest.nix
+    ./14-rust-docker-build-all-publish/rust.nix
+    ./14-rust-docker-build-all-publish/docker.nix
+    ./14-rust-docker-build-all-publish/all.nix
+    ./14-rust-docker-build-all-publish/publish.nix
   ];
 }
