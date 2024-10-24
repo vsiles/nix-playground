3f0f21e1e7744fc159205e67e196d188b349609b
Vincent Siles <vincent.siles@ens-lyon.org>
2024-10-24 20:40:50 +0000

    failed attempt because of specialArgs

--- 30-vm-test/flake.nix
+++ 30-vm-test/flake.nix
@@ -18,22 +18,51 @@
         ./vm.nix
         ./svc-module.nix
       ];
+      linux-pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      darwin-pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+      specialArgs = {
+        inherit axumServer;
+      };
     in
     {
       nixosConfigurations.linuxVM = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = my_modules;
-        specialArgs = {
-          inherit axumServer;
-        };
+        inherit specialArgs;
       };
       packages.x86_64-linux.vm = self.nixosConfigurations.linuxVM.config.system.build.vm;
+      packages.x86_64-linux.test = linux-pkgs.testers.runNixOSTest {
+        name = "VM Test (linux)";
+        inherit specialArgs;
+
+        nodes.machine =
+          { config, pkgs, ... }:
+          {
+            imports = [
+              ./base.nix
+              ./vm.nix
+              ./svc-module.nix
+            ];
+
+            users.users.alice = {
+              isNormalUser = true;
+              extraGroups = [ "wheel" ];
+              packages = [ pkgs.tree ];
+            };
+            system.stateVersion = "24.05";
+          };
+        testScript = ''
+          machine.wait_for_unit("default.target");
+          machine.succeed("su -- alice -c 'which tree'")
+          machine.succeed("su -- test -c 'which jq'")
+        '';
+      };
 
       nixosConfigurations.darwinVM = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
         modules = my_modules ++ [
           # This VM will use the host /nix/store thus avoid 'Exec format error'
-          { virtualisation.vmVariant.virtualisation.host.pkgs = nixpkgs.legacyPackages.aarch64-darwin; }
+          { virtualisation.vmVariant.virtualisation.host.pkgs = darwin-pkgs; }
         ];
         specialArgs = {
           inherit axumServer;
