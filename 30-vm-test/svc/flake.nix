3110e44ab9e65b69a2abba4855d9a10e8bf787f1
Vincent Siles <vincent.siles@helsing.ai>
2024-10-24 15:22:17 +0200

    30-vm-test: better crane

--- 30-vm-test/svc/flake.nix
+++ 30-vm-test/svc/flake.nix
@@ -8,39 +8,73 @@
     crane.url = "github:ipetkov/crane";
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, crane }: flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; overlays = [ rust-overlay.overlays.default ]; };
-      craneLib = crane.mkLib pkgs;
-    in
+  outputs =
     {
- #      packages.default = pkgs.rustPlatform.buildRustPackage {
- #        pname = "axum-echo-server";
- #        version = "0.1.0";
- #        src = ./.;
- #        # cargoLock = ./Cargo.lock;
-	# cargoHash = "sha256-F5/EkZ/s7/fwt4+g9Vfv9Kq16oNt/GIJ/CqdrJ8sSmI=";
- #        buildInputs = [ pkgs.openssl ];
- #      };
-       packages.default = craneLib.buildPackage {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      crane,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+        lib = pkgs.lib;
+        craneLib = crane.mkLib pkgs;
         src = craneLib.cleanCargoSource ./.;
-        buildInputs = [ pkgs.openssl ] ++ 
-         pkgs.lib.optionals pkgs.stdenv.isDarwin [
-          pkgs.libiconv
-        ];
-      };
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
 
-      devShells.default = pkgs.mkShell {
-        buildInputs = [ pkgs.curl pkgs.rustc pkgs.cargo ];
-        shellHook = ''
-          echo 'To test the server with curl:'
-          echo './test_command.sh'
+          buildInputs =
+            [
+              pkgs.openssl
+            ]
+            ++ lib.optionals pkgs.stdenv.isDarwin [
+              pkgs.libiconv
+            ];
+        };
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+      in
+      {
+        packages.default = craneLib.buildPackage (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+          }
+        );
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.curl
+            pkgs.rustc
+            pkgs.cargo
+          ];
+          shellHook = ''
+            echo 'To test the server with curl:'
+            echo './test_command.sh'
+          '';
+        };
+
+        # TODO(vsiles)
+        # add checks, ... https://crane.dev/examples/quick-start.html
+        packages.test_command = pkgs.writeShellScriptBin "test_command" ''
+          curl -X POST -H "Content-Type: application/json" -d '{"message": "Hello, Axum!"}' http://localhost:3000/echo
         '';
-      };
-
-      packages.test_command = pkgs.writeShellScriptBin "test_command" ''
-        curl -X POST -H "Content-Type: application/json" -d '{"message": "Hello, Axum!"}' http://localhost:3000/echo
-      '';
-    }
-  );
+      }
+    );
 }
+
+#      packages.default = pkgs.rustPlatform.buildRustPackage {
+#        pname = "axum-echo-server";
+#        version = "0.1.0";
+#        src = ./.;
+#        # cargoLock = ./Cargo.lock;
+# cargoHash = "sha256-F5/EkZ/s7/fwt4+g9Vfv9Kq16oNt/GIJ/CqdrJ8sSmI=";
+#        buildInputs = [ pkgs.openssl ];
+#      };
