67e61b0d8d7ab6332b35e0557e74dc8ce67c2e6f
Vincent Siles <vincent.siles@helsing.ai>
2024-10-25 08:49:22 +0200

    fix test to be called on macos

--- 30-vm-test/flake.nix
+++ 30-vm-test/flake.nix
@@ -19,7 +19,41 @@
         ./svc-module.nix
       ];
       linux-pkgs = nixpkgs.legacyPackages.x86_64-linux.extend axumServer.overlays.default;
-      darwin-pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+      darwin-pkgs = nixpkgs.legacyPackages.aarch64-darwin.extend axumServer.overlays.default;
+      linux-guest-pkgs = nixpkgs.legacyPackages.aarch64-linux.extend axumServer.overlays.default;
+      tester =
+        {
+          host-pkgs,
+          guest-pkgs,
+          name,
+        }:
+        host-pkgs.testers.runNixOSTest {
+          inherit name;
+
+          nodes.machine =
+            { config, pkgs, ... }:
+            {
+              imports = my_modules;
+
+              users.users.alice = {
+                isNormalUser = true;
+                extraGroups = [ "wheel" ];
+                packages = [ pkgs.tree ];
+              };
+              system.stateVersion = "24.05";
+            };
+          testScript = ''
+            machine.wait_for_unit("default.target");
+            machine.succeed("su -- alice -c 'which tree'")
+            machine.succeed("su -- test -c 'which jq'")
+            result = machine.succeed("ps aux | grep svc")
+            print(result)
+            # Testing GET
+            result = machine.succeed("${guest-pkgs.curl}/bin/curl http://localhost:3000 -X GET")
+            assert result == "Hello, You!"
+
+          '';
+        };
     in
     {
       nixosConfigurations.linuxVM = nixpkgs.lib.nixosSystem {
@@ -29,43 +63,25 @@
         ] ++ my_modules;
       };
       packages.x86_64-linux.vm = self.nixosConfigurations.linuxVM.config.system.build.vm;
-      packages.x86_64-linux.test = linux-pkgs.testers.runNixOSTest {
+      packages.x86_64-linux.test = tester {
+        host-pkgs = linux-pkgs;
+        guest-pkgs = linux-pkgs;
         name = "VM Test (linux)";
-
-        nodes.machine =
-          { config, pkgs, ... }:
-          {
-            imports = my_modules;
-
-            users.users.alice = {
-              isNormalUser = true;
-              extraGroups = [ "wheel" ];
-              packages = [ pkgs.tree ];
-            };
-            system.stateVersion = "24.05";
-          };
-        testScript = ''
-          machine.wait_for_unit("default.target");
-          machine.succeed("su -- alice -c 'which tree'")
-          machine.succeed("su -- test -c 'which jq'")
-          result = machine.succeed("ps aux | grep svc")
-          print(result)
-          # Testing GET
-          result = machine.succeed("${linux-pkgs.curl}/bin/curl http://localhost:3000 -X GET")
-          assert result == "Hello, You!"
-          
-        '';
       };
 
       nixosConfigurations.darwinVM = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
         modules = [
-          # TODO: Maybe overlay darwin-pkgs too / instead ?
           { nixpkgs.overlays = [ axumServer.overlays.default ]; }
           # This VM will use the host /nix/store thus avoid 'Exec format error'
           { virtualisation.vmVariant.virtualisation.host.pkgs = darwin-pkgs; }
         ] ++ my_modules;
       };
       packages.aarch64-darwin.vm = self.nixosConfigurations.darwinVM.config.system.build.vm;
+      packages.aarch64-darwin.test = tester {
+        host-pkgs = darwin-pkgs;
+        guest-pkgs = linux-guest-pkgs;
+        name = "VM Test (darwin)";
+      };
     };
 }
